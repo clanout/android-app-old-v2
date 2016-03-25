a4a6d6cb0e66e8ddd17ebaec3014ade019b6a64c
Harsh Pokharna <harsh.81291@gmail.com>
2016-03-25 18:17:24 +0530

    Notification Screen Layout Changes

--- app/src/main/java/reaper/android/app/ui/screens/notifications/NotificationAdapter.java
+++ app/src/main/java/reaper/android/app/ui/screens/notifications/NotificationAdapter.java
@@ -2,12 +2,15 @@ package reaper.android.app.ui.screens.notifications;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import net.steamcrafted.materialiconlib.MaterialDrawableBuilder;
@@ -18,6 +21,7 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import reaper.android.R;
 import reaper.android.app.model.NotificationWrapper;
+import reaper.android.app.root.Reaper;
 
 public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.NotificationsViewHolder>
 {
@@ -26,12 +30,10 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
     private NotificationClickListener notificationClickListener;
 
     /* Notification Icons */
-    private Drawable chatDrawable;
-    private Drawable rsvpDrawable;
     private Drawable friendAddedDrawable;
-    private Drawable eventUpdatedDrawable;
     private Drawable eventRemovedDrawable;
     private Drawable eventInvitationDrawable;
+    private Drawable alertDrawable;
 
     public NotificationAdapter(Context context, List<NotificationWrapper> notifications, NotificationClickListener notificationClickListener)
     {
@@ -70,14 +72,17 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
 
     public class NotificationsViewHolder extends RecyclerView.ViewHolder
     {
+        @Bind(R.id.llNotificationIconContainer)
+        LinearLayout notificationIconContainer;
+
+        @Bind(R.id.ivNotificationIcon)
+        ImageView notificationIcon;
+
         @Bind(R.id.tvTitle)
         TextView tvTitle;
 
         @Bind(R.id.llItem1)
         View llItem1;
-
-        @Bind(R.id.ivIcon1)
-        ImageView ivIcon1;
 
         @Bind(R.id.tvMessage1)
         TextView tvMessage1;
@@ -85,17 +90,11 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
         @Bind(R.id.llItem2)
         View llItem2;
 
-        @Bind(R.id.ivIcon2)
-        ImageView ivIcon2;
-
         @Bind(R.id.tvMessage2)
         TextView tvMessage2;
 
         @Bind(R.id.llItem3)
         View llItem3;
-
-        @Bind(R.id.ivIcon3)
-        ImageView ivIcon3;
 
         @Bind(R.id.tvMessage3)
         TextView tvMessage3;
@@ -127,87 +126,90 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
             llItem2.setVisibility(View.GONE);
             llItem3.setVisibility(View.GONE);
 
+            ShapeDrawable circle = new ShapeDrawable(new OvalShape());
+            circle.getPaint()
+                    .setColor(ContextCompat.getColor(Reaper.getReaperContext(), R.color.primary_light));
+            notificationIconContainer.setBackground(circle);
+
             NotificationWrapper.NotificationItem item;
             switch (type)
             {
                 case NotificationWrapper.Type.EVENT_ACTIVITY:
+                    notificationIcon.setImageDrawable(alertDrawable);
                     tvTitle.setText(notification.getTitle());
                     int size = notification.getNotificationItems().size();
                     if (size >= 1)
                     {
                         llItem1.setVisibility(View.VISIBLE);
                         item = notification.getNotificationItems().get(0);
-                        renderItem(item, ivIcon1, tvMessage1);
+                        renderItem(item, tvMessage1);
                     }
                     if (size >= 2)
                     {
                         llItem2.setVisibility(View.VISIBLE);
                         item = notification.getNotificationItems().get(1);
-                        renderItem(item, ivIcon2, tvMessage2);
+                        renderItem(item, tvMessage2);
                     }
                     if (size >= 3)
                     {
                         llItem3.setVisibility(View.VISIBLE);
                         item = notification.getNotificationItems().get(2);
-                        renderItem(item, ivIcon3, tvMessage3);
+                        renderItem(item, tvMessage3);
                     }
                     break;
 
                 case NotificationWrapper.Type.EVENT_INVITATION:
+                    notificationIcon.setImageDrawable(eventInvitationDrawable);
                     tvTitle.setText(notification.getTitle());
                     llItem1.setVisibility(View.VISIBLE);
                     item = notification.getNotificationItems().get(0);
-                    renderItem(item, ivIcon1, tvMessage1);
+                    renderItem(item, tvMessage1);
                     break;
 
                 case NotificationWrapper.Type.EVENT_REMOVED:
+                    notificationIcon.setImageDrawable(eventRemovedDrawable);
                     tvTitle.setText(notification.getTitle());
                     llItem1.setVisibility(View.VISIBLE);
                     item = notification.getNotificationItems().get(0);
-                    renderItem(item, ivIcon1, tvMessage1);
+                    renderItem(item, tvMessage1);
                     break;
 
                 case NotificationWrapper.Type.NEW_FRIEND_JOINED_APP:
+                    notificationIcon.setImageDrawable(friendAddedDrawable);
                     tvTitle.setText("New Friends");
                     llItem1.setVisibility(View.VISIBLE);
                     item = notification.getNotificationItems().get(0);
-                    renderItem(item, ivIcon1, tvMessage1);
+                    renderItem(item, tvMessage1);
                     break;
             }
         }
 
-        private void renderItem(NotificationWrapper.NotificationItem item, ImageView ivIcon, TextView tvMessage)
+        private void renderItem(NotificationWrapper.NotificationItem item, TextView tvMessage)
         {
             int type = item.getType();
             switch (type)
             {
                 case NotificationWrapper.NotificationItem.Type.EVENT_REMOVED:
-                    ivIcon.setImageDrawable(eventRemovedDrawable);
                     tvMessage.setText("This clan has been removed");
                     break;
 
                 case NotificationWrapper.NotificationItem.Type.NEW_FRIEND_JOINED_APP:
-                    ivIcon.setImageDrawable(friendAddedDrawable);
                     tvMessage.setText(item.getMessage());
                     break;
 
                 case NotificationWrapper.NotificationItem.Type.INVITATION:
-                    ivIcon.setImageDrawable(eventInvitationDrawable);
                     tvMessage.setText(item.getMessage());
                     break;
 
                 case NotificationWrapper.NotificationItem.Type.EVENT_UPDATED:
-                    ivIcon.setImageDrawable(eventUpdatedDrawable);
                     tvMessage.setText("Details updated");
                     break;
 
                 case NotificationWrapper.NotificationItem.Type.NEW_CHAT:
-                    ivIcon.setImageDrawable(chatDrawable);
                     tvMessage.setText("New chat");
                     break;
 
                 case NotificationWrapper.NotificationItem.Type.FRIEND_JOINED_EVENT:
-                    ivIcon.setImageDrawable(rsvpDrawable);
                     tvMessage.setText("New friends have joined");
                     break;
             }
@@ -216,45 +218,32 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
 
     private void generateDrawables()
     {
-        chatDrawable = MaterialDrawableBuilder
-                .with(context)
-                .setIcon(MaterialDrawableBuilder.IconValue.MESSAGE_TEXT)
-                .setColor(ContextCompat.getColor(context, R.color.primary_light))
-                .build();
-
-        rsvpDrawable = MaterialDrawableBuilder
-                .with(context)
-                .setIcon(MaterialDrawableBuilder.IconValue.ACCOUNT_MULTIPLE_PLUS)
-                .setColor(ContextCompat
-                        .getColor(context, R.color.primary_light))
-                .build();
-
         friendAddedDrawable = MaterialDrawableBuilder
                 .with(context)
                 .setIcon(MaterialDrawableBuilder.IconValue.ACCOUNT_PLUS)
                 .setColor(ContextCompat
-                        .getColor(context, R.color.primary_light))
-                .build();
-
-        eventUpdatedDrawable = MaterialDrawableBuilder
-                .with(context)
-                .setIcon(MaterialDrawableBuilder.IconValue.TABLE_EDIT)
-                .setColor(ContextCompat
-                        .getColor(context, R.color.primary_light))
+                        .getColor(context, R.color.white))
                 .build();
 
         eventRemovedDrawable = MaterialDrawableBuilder
                 .with(context)
                 .setIcon(MaterialDrawableBuilder.IconValue.CALENDAR_REMOVE)
                 .setColor(ContextCompat
-                        .getColor(context, R.color.primary_light))
+                        .getColor(context, R.color.white))
                 .build();
 
         eventInvitationDrawable = MaterialDrawableBuilder
                 .with(context)
                 .setIcon(MaterialDrawableBuilder.IconValue.EMAIL_OPEN)
                 .setColor(ContextCompat
-                        .getColor(context, R.color.primary_light))
+                        .getColor(context, R.color.white))
+                .build();
+
+        alertDrawable = MaterialDrawableBuilder
+                .with(context)
+                .setIcon(MaterialDrawableBuilder.IconValue.ALERT_CIRCLE)
+                .setColor(ContextCompat
+                        .getColor(context, R.color.white))
                 .build();
     }
 }
